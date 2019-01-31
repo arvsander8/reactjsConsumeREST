42ef30b3eb79bd1fbec9033ca69ed32af41056ce
arvsander8 <arv.sander9@gmail.com>
2019-01-30 23:14:57 -0500

    Added the POST example, updated code and add bootstrap components

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # reactjsConsumeREST
+
 ## API GETWAY Configuration
 
 ### API Methods configuration to enabled CORS Headers
@@ -9,9 +10,7 @@
     1. Select a method to assigne the headers access
 
     ![Alt paso1](imgs/reactjsConsumeREST_2.png)
-
     2. In the right opened panel, choose ***Respuesta de Método***
-    
     ![Alt paso2](imgs/reactjsConsumeREST_3.png)
     * Expand the *200* option from the ***Estado HTTP***
         * Click on ***Agregar encabezado***
@@ -21,13 +20,13 @@
                 * `Access-Control-Allow-Headers`
                 * `Access-Control-Allow-Origin`
                 * `Access-Control-Allow-Methods`
-                ![Alt paso2](imgs/reactjsConsumeREST_5.png)
 
+    ![Alt paso2](imgs/reactjsConsumeREST_5.png)
     3. In the right panel, choose ***Respuestas de Integración***
-    ![Alt paso2](imgs/reactjsConsumeREST_6.png)
+        ![Alt paso2](imgs/reactjsConsumeREST_6.png)
 
     * Expand the *row with status 200*
-        * Expand ***Mapeos de encabezado*** 
+        * Expand ***Mapeos de encabezado***
         ![Alt paso2](imgs/reactjsConsumeREST_7.png)
 
         * Add the mapping value (***Valor de mapeo***) to all options like that and save changes:
@@ -39,8 +38,6 @@
         Access-Control-Allow-Origin   | '*'
         Access-Control-Allow-Methods  | 'POST, GET, OPTIONS'
 
-        ![Alt paso2](imgs/reactjsConsumeREST_8.png)
-
-
+    ![Alt paso2](imgs/reactjsConsumeREST_8.png)
     4. Finally, Deploy again the API
     ![Alt paso2](imgs/reactjsConsumeREST_9.png)
--- clientes-app/src/App.js
+++ clientes-app/src/App.js
@@ -1,20 +1,51 @@
 import React, { Component } from 'react';
-import {Table, Button} from 'reactstrap';
+import { Input, FormGroup, Label, Modal, ModalHeader, ModalBody, ModalFooter, Table, Button } from 'reactstrap';
+import axios from 'axios';
 
 class App extends Component {
 
   constructor(props){
     super(props);
     this.state = {
-      clientes: [],
-      isLoaded: false
+      clients: [],
+      newClientData:{
+        id: 2,
+        nombre: '',
+        apellido: ''
+      },
+      newClientModal: false
     }
 
   }
 
+
+  toggleNewClientModal() {
+    this.setState({
+      newClientModal: ! this.state.newClientModal
+    });
+  }
+
+  addClient() {
+    axios.post('https://epgk5i5g2h.execute-api.us-east-1.amazonaws.com/desarrollo/leertabla', this.state.newClientData)
+      .then((response) => {
+        let { clients } = this.state;
+
+        clients.push(response.data);
+
+      this.setState({ clients, newClientModal: false, newClientData: {
+        id: 2,
+        nombre: '',
+        apellido: ''
+      }});
+    });
+  }
   
-  componentDidMount(){
-    fetch('https://epgk5i5g2h.execute-api.us-east-1.amazonaws.com/desarrollo/leertabla')
+  componentWillMount(){
+    this._refreshClients();
+  }
+
+  _refreshClients(){
+          /*fetch('https://epgk5i5g2h.execute-api.us-east-1.amazonaws.com/desarrollo/leertabla')
       .then( res => res.json())
       .then( res => {
           this.setState({
@@ -24,19 +55,24 @@ class App extends Component {
       })
       .catch( error => {
         console.log(error);
-      }) 
+      }) */
 
+      axios.get('https://epgk5i5g2h.execute-api.us-east-1.amazonaws.com/desarrollo/leertabla')
+        .then((response) => {
+          this.setState({
+            clients: response.data
+          })
+        });
   }
-
 
   render() {
 
-    let clientes = this.state.clientes.map((cliente) => {
+    let clients = this.state.clients.map((client) => {
       return(
-        <tr key={cliente.id}>
-              <td>{cliente.id}</td>
-              <td>{cliente.nombre}</td>
-              <td>{cliente.apellido}</td>
+        <tr key={client.id}>
+              <td>{client.id}</td>
+              <td>{client.nombre}</td>
+              <td>{client.apellido}</td>
               <td>
                 <Button color="success" size='sm' className="mr-2">Edit</Button>
                 <Button color="danger" size='sm'>Delete</Button>
@@ -47,8 +83,54 @@ class App extends Component {
 
     
     return (
-      <div className="App">
+      <div className="App container">
+        <h1>Ejemplo de Clientes</h1>
         
+        <Button className="my-3" color="primary" onClick={this.toggleNewClientModal.bind(this)}>Nuevo Cliente</Button>
+
+
+        <Modal isOpen={this.state.newClientModal} toggle={this.toggleNewClientModal.bind(this)}>
+          <ModalHeader toggle={this.toggleNewClientModal.bind(this)}>Agregar Cliente</ModalHeader>
+          <ModalBody>
+          <FormGroup>
+              <Label for="ide">Id</Label>
+              <Input id="ide" value={this.state.newClientData.id} onChange={(e) => {
+                let { newClientData } = this.state;
+
+                newClientData.id = e.target.value;
+
+                this.setState({ newClientData });
+              }} />
+            </FormGroup>
+            <FormGroup>
+              <Label for="nombre">Nombres</Label>
+              <Input id="nombre" value={this.state.newClientData.nombre} onChange={(e) => {
+                let { newClientData } = this.state;
+
+                newClientData.nombre = e.target.value;
+
+                this.setState({ newClientData });
+              }} />
+            </FormGroup>
+            <FormGroup>
+              <Label for="apellido">Apellidos</Label>
+              <Input id="apellido" value={this.state.newClientData.apellido} onChange={(e) => {
+                let { newClientData } = this.state;
+
+                newClientData.apellido = e.target.value;
+
+                this.setState({ newClientData });
+              }} />
+            </FormGroup>
+
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={this.addClient.bind(this)}>Agregar Cliente</Button>{' '}
+            <Button color="secondary" onClick={this.toggleNewClientModal.bind(this)}>Cancel</Button>
+          </ModalFooter>
+        </Modal>
+
+
         <Table>
           <thead>
             <tr>
@@ -59,7 +141,7 @@ class App extends Component {
             </tr>
           </thead>
           <tbody>
-            {clientes}
+            {clients}
           </tbody>
         </Table>
       </div>
